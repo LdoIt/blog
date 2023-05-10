0450c068ea77181157abb1773d1e1c1dd35d44ef
Good <1204889603@qq.com>
2023-05-10 13:58:22 +0800

    部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,10 @@
 module.exports = {
 	title: 'Good',
 	description: 'Just playing around',
+	base: '/blog/',
 	themeConfig: {
-		sidebarDepth: 2, // 显示标题链接的层数，最大为2，最小为0
+		lastUpdated: 'Last Updated', // 最后提交时间
+		// sidebarDepth: 2, // 显示标题链接的层数，最大为2，最小为0
 		displayAllHeaders: true, // 显示所有页面的标题链接
 		activeHeaderLinks: false, // 当用户滚动查看页面时阻止标题链接和 URL 中的 Hash 值实时更新行为
 		logo: '/assets/img/logo.png',
@@ -19,13 +21,17 @@ module.exports = {
 				link: '/javascript/'
 			},
 			{
-				text: 'Languages',
+				text: '知识点',
+				link: '/knowledge/'
+			},
+			{
+				text: '语言',
 				items: [{
-						text: 'Chinese',
+						text: '中文',
 						link: '/language/chinese/'
 					},
 					{
-						text: 'Japanese',
+						text: '日文',
 						link: '/language/japanese/'
 					}
 				]
@@ -33,20 +39,20 @@ module.exports = {
 		],
 		sidebar: {
 			'/html/': [
-				'', /* /html/ */
-				'one', /* /html/one.html */
-				'two' /* /html/two.html */
+				{title: 'Group1', children: ['']},
+				{title: 'Group2', children: ['one', 'two']}
 			],
-
 			'/css/': [
-				'',
-				'three', /* /css/three.html */
-				'four' /* /css/four.html */
+				{title: 'Group1', children: ['']},
+				{title: 'Group2', children: ['three', 'four']}
 			],
-
-			// fallback
 			'/javascript/': [
 				''
+			],
+			'/knowledge/': [
+				'',
+				'closure',
+				'sort'
 			]
 		}
 	},
